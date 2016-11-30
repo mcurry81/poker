6bf480c5de5b77893b9c412342da54f3859f99c2
Patricia Buckenberger (InConsulting Inc.) <v-pabuck@microsoft.com>
2016-11-30 12:40:49 -0800

    adding Card and Game Classes

--- Poker/Poker/Card.cs
+++ Poker/Poker/Card.cs
@@ -20,78 +20,9 @@ namespace Poker
             this.rank = rank;
         }
 
-        //card constructor for manual construction
-        public Card(string str)
-        {
-            str = str.ToUpper();
-            foreach (char c in str)
-            {
-                switch (c)
-                {
-                    case 'C':
-                    case 'D':
-                    case 'H':
-                    case 'S':
-                        suit = c;
-                        break;
-                    case 'T':
-                        rank = 10;
-                        break;
-                    case 'Q':
-                        rank = 12;
-                        break;
-                    case 'K':
-                        rank = 13;
-                        break;
-                    case 'J':
-                        rank = 11;
-                        break;
-                    case 'A':
-                        rank = 14;
-                        break;
-                    case '9':
-                    case '8':
-                    case '7':
-                    case '6':
-                    case '5':
-                    case '4':
-                    case '3':
-                    case '2':
-                        rank = c - '0';
-                        break;
-
-
-                }
-            }
-            if (rank == 0)
-                Console.WriteLine("Hey dummy, please give your card's rank from " + str);
-
-            //changed this from if(suit == 0) to comply more strictly with C# and prevent any possible future errors
-            if (suit == '\0')
-                Console.WriteLine("Hey dummy, please enter your suit from " + str);
-
-        }
-
         bool isValid()
         {
             return suit != '\0' && rank >= 2 && rank <= 14;
-        }
-
-        public int CompareTo(object obj)
-        {
-            Card c = obj as Card;
-            return rank - c.rank;
-            throw new NotImplementedException();
-        }
-
-        public int getRank()
-        {
-            return rank;
-        }
-
-        public char getSuit()
-        {
-            return suit;
         }
     }
 }
---
+++ Poker/Poker/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    class Game
+    {
+        public Player player;
+        public Deck currentDeck = new Deck();
+        public Player house = new Player();
+        protected int minCardsNeeded = 10;
+        protected int numCardsToDeal = 5;
+
+        public Game(Player player)
+        {
+            this.player = player;
+        }
+
+        //Begins a new game and returns the winner
+        public Game StartGame()
+        {
+            if (currentDeck.count < minCardsNeeded)
+            {
+                currentDeck = new Poker.Deck();
+            }
+            for (int i = 0; i < numCardsToDeal; i++)
+            {
+                currentDeck.dealCard(player);
+                currentDeck.dealCard(house);
+            }
+            if (house.hand.getValue() > player.hand.getValue())
+                return house;
+            else
+                return player;
+        }      
+    }
+}
