c6d65a630225c1e1985b4fd96e17a5138497d095
v-mecurr <v-mecurr@MININT-7EQEO88>
2016-12-06 16:50:12 -0800

    made suit and rank enums public, some test methods.

--- Poker/Poker/Test.cs
+++ Poker/Poker/Test.cs
@@ -11,19 +11,23 @@ namespace Poker
         //test Deck Class
         //create a test deck
         Deck testDeck = new Deck(5);
+        Card testCard;
 
     
         //test printAllCards()
         public void testPrintAll(Deck deck)
         {
-
-                Console.WriteLine("printAllCards() passed test;");
-
-
+            int count = deck.Length();
+            deck.printAllCards();
+            Console.WriteLine("printAllCards method completed run...sucessful if " + count + " cards printed.");
         }
 
         //test dealCard()
+        public Card testDealCard(Deck deck)
+        {
+            testCard = new Poker.Card();
 
+        }
         //test shuffle()
 
 
